82a19694c873f580d818c4fee9fa14ff6b50cb8d
Nathan Pickrell <npickrel@tomcat.cecs.csulb.edu>
2012-10-05 19:08:48 -0700

    Added the homesfix script to the appendix.

---
+++ appendix_material/homesfix.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This script takes in a file of student homes, then cds to each.
+# mv .xinitrc .xinitrc.bak
+v_homefile="$1"
+v_filetomv="./.xinitrc"
+v_logfile="/tmp/mvlog.log"
+
+while read homedir
+do
+  echo "Moving to $homedir" >> "$v_logfile" 2>&1
+  cd "$homedir"  >> "$v_logfile" 2>&1
+  if [ -e "$v_filetomv" ];then
+    echo "mving file $v_filetomv to ${v_filetomv}.bak..." >> "$v_logfile" 2>&1
+    mv "${v_filetomv}" "${v_filetomv}".bak >> "$v_logfile" 2>&1
+  else
+    echo "$v_filetomv in $homedir not found." >> "$v_logfile" 2>&1
+  fi
+done < "$v_homefile"
